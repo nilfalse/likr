daa9fb4cd5f027596238dd62eb30051185ddbdb9
Dmytro <plekhotkindmytro@gmail.com>
2015-04-04 21:18:03 +0300

    Update

--- public/js/game.js
+++ public/js/game.js
@@ -1,9 +1,18 @@
 var socket = io();
 
-socket.emit("game join", getQueryParams("_id"));
+socket.emit("game join", getQueryParams("gameId"));
+
+socket.on("game joined", function(game) {
+	$('#gameHeader').html(game.name);
+	console.log(game.players);
+});
 
 socket.on("player joined", function(player) {
 	var template = _($('#playerItemTemplate').html()).template();
 	var playerHtml = template(player);
 	$('#playerList').append(playerHtml);
+});
+
+socket.on("error", function(error) {
+	console.error(error);
 });
