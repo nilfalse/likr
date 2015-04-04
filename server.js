cfbc9157cf04bddb0dc9556039adfdd385246994
Yaroslav <nilfalse@ya.ru>
2015-04-04 15:23:51 +0300

    getting started with socket.io

--- server.js
+++ server.js
@@ -9,18 +9,28 @@ app.use(require('morgan')('combined'));
 // parse application/json 
 app.use(require('body-parser').json());
 
+var gamesList = [];
 
-io.on('connection', function(socket){
-  console.log('a user connected');
+io.on('connection', function(socket) {
+	socket.on('disconnect', function() {
+	});
+
+	socket.emit('games', gamesList);
+	socket.on('create game', function(game) {
+		gamesList.push(game);
+		console.log('user created game');
+	});
+	socket.on('join game', function(game) {
+		console.log('user joined game');
+	});
 });
 
 app.get('/', function(req, res) {
 	res.send({ response: 'hello' });
 });
 
-var server = app.listen(process.env.PORT || 3000, function() {
-	var host = server.address().address;
-	var port = server.address().port;
-	console.log('listening at http://%s:%s', host, port)
+var port = process.env.PORT || 3000;
+http.listen(port, function(){
+	console.log('listening to port', port);
 });
 
