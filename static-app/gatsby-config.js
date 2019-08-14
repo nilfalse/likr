f1751730c666d6242b0c4b3be323fbf0ee399a99
Yaroslav <nilfalse@yandex.com>
2019-08-14 18:17:45 +0200

    add empty Gatsby app

---
+++ static-app/gatsby-config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  siteMetadata: {
+    title: `likr`,
+    description: `Find out how well you match with another person.`,
+    author: `@nilfalse`,
+  },
+  plugins: [
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+      },
+    },
+  ],
+};
---
+++ static-app/src/components/Layout/index.js
@@ -0,0 +1,3 @@
+import './Layout.css';
+
+export * from './Layout';
